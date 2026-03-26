1893593dd094312fc9f72fa7640c6a00183ada9f
dremchee <dremchee@gmail.com>
2026-03-26 11:05:24 +0300

    style(plugin): align scope settings markup

--- plugin/src/settings/components/SettingsScopeSection.vue
+++ plugin/src/settings/components/SettingsScopeSection.vue
@@ -1,38 +1,44 @@
 <script setup lang="ts">
-import { t } from "@/i18n";
-import type {
-  SettingsScopeActions,
-  SettingsScopeViewModel,
-} from "../view-model";
+  import { t } from '@/i18n'
+  import type {
+    SettingsScopeActions,
+    SettingsScopeViewModel
+  } from '../view-model'
 
-const props = defineProps<{
-  model: SettingsScopeViewModel;
-  actions: SettingsScopeActions;
-}>();
+  const props = defineProps<{
+    model: SettingsScopeViewModel
+    actions: SettingsScopeActions
+  }>()
 
-function handleIncludePatternsInput(event: Event): void {
-  props.actions.onIncludePatternsChange((event.currentTarget as HTMLTextAreaElement).value);
-}
+  function handleIncludePatternsInput(event: Event): void {
+    props.actions.onIncludePatternsChange(
+      (event.currentTarget as HTMLTextAreaElement).value
+    )
+  }
 
-function handleIgnorePatternsInput(event: Event): void {
-  props.actions.onIgnorePatternsChange((event.currentTarget as HTMLTextAreaElement).value);
-}
+  function handleIgnorePatternsInput(event: Event): void {
+    props.actions.onIgnorePatternsChange(
+      (event.currentTarget as HTMLTextAreaElement).value
+    )
+  }
 </script>
 
 <template>
   <div class="setting-group">
     <div class="setting-item setting-item-heading">
-      <div class="setting-item-name">{{ t("settings.scope.heading") }}</div>
+      <div class="setting-item-name">{{ t('settings.scope.heading') }}</div>
     </div>
     <div class="setting-items">
       <div class="setting-item">
         <div class="setting-item-info">
-          <div class="setting-item-name">{{ t("settings.scope.include.label") }}</div>
+          <div class="setting-item-name">
+            {{ t('settings.scope.include.label') }}
+          </div>
           <div class="setting-item-description">
-            {{ t("settings.scope.include.description") }}
+            {{ t('settings.scope.include.description') }}
           </div>
         </div>
-        <div class="setting-item-control obsidian-sync-textarea-control">
+        <div class="setting-item-control">
           <textarea
             :value="props.model.includePatterns.join('\n')"
             :placeholder="t('settings.scope.include.placeholder')"
@@ -45,12 +51,14 @@ function handleIgnorePatternsInput(event: Event): void {
 
       <div class="setting-item obsidian-sync-with-top-border">
         <div class="setting-item-info">
-          <div class="setting-item-name">{{ t("settings.scope.ignore.label") }}</div>
+          <div class="setting-item-name">
+            {{ t('settings.scope.ignore.label') }}
+          </div>
           <div class="setting-item-description">
-            {{ t("settings.scope.ignore.description") }}
+            {{ t('settings.scope.ignore.description') }}
           </div>
         </div>
-        <div class="setting-item-control obsidian-sync-textarea-control">
+        <div class="setting-item-control">
           <textarea
             :value="props.model.ignorePatterns.join('\n')"
             :placeholder="t('settings.scope.ignore.placeholder')"
